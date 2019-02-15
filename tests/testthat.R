353ff900f4b25967f4b9225cb3b365ab0c933448
Shawn McKenzie <pfshawn@gmail.com>
2019-02-14 23:31:11 -0800

    Test to fail check

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,5 @@
 library(testthat)
 library(fars)
 
-test_that("sumyrs",expect_that(fars_summarize_years(2013:2015),is_a("tbl_df")))
+test_that("sumyrs",expect_that(fars_summarize_years(2013:2015),is_a("character")))
 # test_check("fars")
