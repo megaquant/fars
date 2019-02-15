60c26d1037df64df3fbdad068350beb953dc1953
Shawn McKenzie <pfshawn@gmail.com>
2019-02-15 00:03:52 -0800

    Fix tests dir structure

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,4 @@
 library(testthat)
 library(fars)
 
-test_that("sumyrs",expect_that(fars_summarize_years(2013:2015),is_a("character")))
-# test_check("fars")
+test_check("fars")
---
+++ tests/testthat/tests.R
@@ -0,0 +1,1 @@
+test_that("sumyrs",expect_that(fars_summarize_years(2013:2015),is_a("tbl_df")))
